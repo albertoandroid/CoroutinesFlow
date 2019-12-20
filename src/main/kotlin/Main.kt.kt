808290f61f9f05de701f4d3e776aa43ec2c61501
Alberto <info@androiddesdecero.com>
2019-12-20 14:21:13 +0100

    2

--- src/main/kotlin/Main.kt.kt
+++ src/main/kotlin/Main.kt.kt
@@ -1,10 +1,24 @@
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>){
+    /*
     //show()
     runBlocking {
         runAsynchronous().forEach{i-> println(i)}
+    }
+
+     */
+    runBlocking {
+        launch {
+            for(j in 1..3){
+                println("No estoy bloqueado $j")
+                kotlinx.coroutines.delay(1000)
+            }
+        }
+        firstFlow().collect{value->println(value)}
     }
 }
 
@@ -26,5 +40,12 @@ suspend fun runAsynchronous(): List<Int>{
     return runBlocking {
         kotlinx.coroutines.delay(1000)
         return@runBlocking listOf(1,2,3)
+    }
+}
+
+fun firstFlow(): Flow<Int> = flow{
+    for(i in 1..3){
+        kotlinx.coroutines.delay(1000)
+        emit(i)
     }
 }
