1aa9cab65ee39544e8a2105cbc4e61904497b88f
Alberto <info@androiddesdecero.com>
2019-12-20 16:06:28 +0100

    3

--- src/main/kotlin/Main.kt.kt
+++ src/main/kotlin/Main.kt.kt
@@ -53,7 +53,7 @@ fun main(args: Array<String>){
             .collect{response-> println(response)}
     }
 
-     */
+
 
     runBlocking {
         (1..3).asFlow()
@@ -61,6 +61,28 @@ fun main(args: Array<String>){
             .map{request->performRequest(request)}
             .collect{response->println(response)}
     }
+
+
+
+    runBlocking {
+        (1..3).asFlow()
+            .transform{
+                request->
+                    emit("Making request $request")
+                    emit(performRequest(request))
+            }
+            .collect{
+                response->println(response)
+            }
+
+    }
+    */
+    runBlocking {
+        (1..3).asFlow()
+            .take(2)
+            .collect{response->println(response)}
+    }
+
 }
 
 fun show(){
